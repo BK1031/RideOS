e6b5619fa62064c56b331fcb0a2934c1c427ae71
Bharat Kathi <bharat1031@gmail.com>
2024-07-22 21:01:30 -0700

    reset

--- android/app/src/main/java/com/bk1031/ride_os/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.bk1031.ride_os;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- lib/models/user.dart
+++
@@ -1,6 +0,0 @@
-class User {
-  String id = "Akz9x02jmS90d02kD9";
-  String firstName = "Bharat";
-  String lastName = "Kathi";
-  String email = "bharat1031@gmail.com";
-}
